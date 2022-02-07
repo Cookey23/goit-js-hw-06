eb8fd8cabeb63b0fdcd3c7366d3dee3c5f1a6d7d
Serhii Alavatskyi <alavatsky@gmail.com>
2022-02-08 00:30:20 +0200

    task 9 complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,9 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeBtnEl = document.querySelector('.change-color');
+const valueColorEl = document.querySelector('.color');
+changeBtnEl.addEventListener('click', event => {
+  const newColor = document.body.style.backgroundColor = getRandomHexColor();
+  valueColorEl.textContent = newColor;
+});
