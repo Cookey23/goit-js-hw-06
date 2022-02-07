9b6c8fce647cfc88e1ae3c8cb6a558b9fe5d0722
Serhii Alavatskyi <alavatsky@gmail.com>
2022-02-07 22:30:33 +0200

    task 6 done

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const textInput = document.querySelector('#validation-input');
+textInput.addEventListener('blur', changeClass);
+function changeClass() {
+    textInput.classList.add('invalid')
+    if (textInput.value.length == textInput.dataset.length) {
+        textInput.classList.replace('invalid', 'valid')
+    }
+}
+ 
